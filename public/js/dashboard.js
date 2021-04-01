6bc82fcf24608960087271af1d28991e45a54ef3
Mike Coletta <coletta.mike@gmail.com>
2021-03-31 19:51:17 -0700

    Fixed issue where multiple post edit/delete buttons do not work

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,63 +1,59 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#post-title').value.trim();
-    const body = document.querySelector('#post-body').value.trim();
-  
-    if (title && body) {
-      const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({ title, body }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create blog post');
-      }
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const body = document.querySelector('#post-body').value.trim();
+
+  if (title && body) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, body }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create blog post');
     }
-  };
-  
-  const editButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-      document.location.replace(`/edit/post/${id}`)
+  }
+};
+
+const editButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    document.location.replace(`/edit/post/${id}`)
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete blog post');
     }
-  };
+  }
+};
 
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete blog post');
-      }
-    }
-  };
-  
-  document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
 
+let editButtons = document.querySelectorAll('.edit-button')
+editButtons.forEach((btn) => {
+  btn.addEventListener("click", editButtonHandler)
+});
 
-const editButton =  document.querySelector('#edit-button')
-
-if (editButton !== null) {
-  editButton.addEventListener('click', editButtonHandler);
-}
-
-const deleteButton =  document.querySelector('#delete-button')
-
-if (deleteButton !== null) {
-  deleteButton.addEventListener('click', delButtonHandler);
-}
-  
+let deleteButtons = document.querySelectorAll('.delete-button')
+deleteButtons.forEach((btn) => {
+  btn.addEventListener("click", delButtonHandler)
+});
--- public/js/post.js
+++ public/js/post.js
@@ -1,30 +1,30 @@
 const newCommentHandler = async (event) => {
-    event.preventDefault();
-  
-    const body = document.querySelector('#comment-body').value.trim();
-    const postID = +window.location.pathname.split("/")[2]
-    const commentData = {
-      body,
-      post_id: postID,
-    };
+  event.preventDefault();
 
-    if (body) {
-      const response = await fetch(`/api/comments`, {
-        method: 'POST',
-        body: JSON.stringify( commentData ),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert('Failed to add comment');
-      }
-    }
+  const body = document.querySelector('#comment-body').value.trim();
+  const postID = +window.location.pathname.split("/")[2]
+  const commentData = {
+    body,
+    post_id: postID,
   };
 
-  document
-    .querySelector('.new-comment-form')
-    .addEventListener('submit', newCommentHandler);
+  if (body) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify(commentData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Failed to add comment');
+    }
+  }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newCommentHandler);
